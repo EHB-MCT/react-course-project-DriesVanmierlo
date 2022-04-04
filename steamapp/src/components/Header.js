da25184b07182e523c9fd8e1aabc6fa4a054b98a
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2022-04-04 13:22:54 +0200

    WIP added CSS to the header

--- steamapp/src/components/Header.js
+++ steamapp/src/components/Header.js
@@ -7,26 +7,28 @@ function HeaderNav(){
     });
 
     return (
-        <div>
-            <figure>
-                <img src="https://store.akamai.steamstatic.com/public/shared/images/header/logo_steam.svg?t=962016" />
-            </figure>
-            <ul>
-                {list}
-            </ul>
-            <form>
-                <input placeholder="Zoek..."/>
-                <input type="button" value="Zoek"/>
-            </form>
-            <button>Download</button>
+        <div className="header-nav">
+            <div className="header-nav-container">
+                <figure className="header-nav-figure">
+                    <img src="https://store.akamai.steamstatic.com/public/shared/images/header/logo_steam.svg?t=962016" />
+                </figure>
+                <ul className="header-nav-list">
+                    {list}
+                </ul>
+                <form className="header-nav-search">
+                    <input id="search-input" placeholder="Zoek..."/>
+                    <input id="search-button" type="button" value="Zoek"/>
+                </form>
+                <button id="download-button">Download</button>
+            </div>
         </div>
     );
 }
 
 function HeaderBanner (){
     return (
-        <div>
-            <figure>
+        <div className="header-banner-container">
+            <figure className="header-banner-figure">
                 <img src="https://www.gamepitt.co.uk/wp-content/uploads/2021/03/it-takes-two-banner.jpg" />
             </figure>
         </div>
